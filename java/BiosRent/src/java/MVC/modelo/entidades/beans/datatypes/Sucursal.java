69099a90584e6e355434b641b67b42a90f8181a7
Facundo Mora Jara <kufamora@gmail.com>
2019-06-11 19:45:52 -0300

    Implementacion de beans de Sucursal y Vehiculo

--- java/BiosRent/src/java/MVC/modelo/entidades/beans/datatypes/Sucursal.java
+++ java/BiosRent/src/java/MVC/modelo/entidades/beans/datatypes/Sucursal.java
@@ -10,5 +10,38 @@ package MVC.modelo.entidades.beans.datatypes;
  * @author Nicolas
  */
 public class Sucursal {
+    //Atributos
     
+    private int _codigo;
+    private String _nombre;
+
+    //Constructores
+    public Sucursal() { }
+
+    public Sucursal(int _codigo, String _nombre) {
+        this._codigo = _codigo;
+        this._nombre = _nombre;
+    }
+
+    //Propiedades
+    public int getCodigo() {
+        return _codigo;
+    }
+
+    public void setCodigo(int _codigo) {
+        this._codigo = _codigo;
+    }
+
+    public String getNombre() {
+        return _nombre;
+    }
+    
+    public void setNombre(String _nombre) {
+        this._nombre = _nombre;
+    }
+    
+    @Override
+    public String toString() {
+        return "Sucursal{" + "_codigo=" + _codigo + ", _nombre=" + _nombre + '}';
+    }
 }
--- java/BiosRent/src/java/MVC/modelo/entidades/beans/datatypes/Vehiculo.java
+++ java/BiosRent/src/java/MVC/modelo/entidades/beans/datatypes/Vehiculo.java
@@ -11,4 +11,68 @@ package MVC.modelo.entidades.beans.datatypes;
  */
 public class Vehiculo {
     
+    //Atributos
+    private String _matricula;
+    private String _tipo;
+    private String _descripcion;
+    private double _precioAlquilerDiario;
+    
+    private Sucursal _sucursalPertenece;
+
+    //Constructores
+    public Vehiculo(String _matricula, String _tipo, String _descripcion, double _precioAlquilerDiario, Sucursal _sucursalPertenece) {
+        this._matricula = _matricula;
+        this._tipo = _tipo;
+        this._descripcion = _descripcion;
+        this._precioAlquilerDiario = _precioAlquilerDiario;
+        this._sucursalPertenece = _sucursalPertenece;
+    }
+
+    public Vehiculo() { }
+
+    //Propiedades
+    public String getMatricula() {
+        return _matricula;
+    }
+
+    public void setMatricula(String _matricula) {
+        this._matricula = _matricula;
+    }
+
+    public String getTipo() {
+        return _tipo;
+    }
+
+    public void setTipo(String _tipo) {
+        this._tipo = _tipo;
+    }
+
+    public String getDescripcion() {
+        return _descripcion;
+    }
+
+    public void setDescripcion(String _descripcion) {
+        this._descripcion = _descripcion;
+    }
+
+    public double getPrecioAlquilerDiario() {
+        return _precioAlquilerDiario;
+    }
+
+    public void setPrecioAlquilerDiario(double _precioAlquilerDiario) {
+        this._precioAlquilerDiario = _precioAlquilerDiario;
+    }
+    
+    public Sucursal getSucursalPertenece() {
+        return _sucursalPertenece;
+    }
+
+    public void setSucursalPertenece(Sucursal _sucursalPertenece) {
+        this._sucursalPertenece = _sucursalPertenece;
+    }
+    
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "_matricula=" + _matricula + ", _tipo=" + _tipo + ", _descripcion=" + _descripcion + ", _precioAlquilerDiario=" + _precioAlquilerDiario + '}';
+    }
 }

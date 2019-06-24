a33584a6cea924d02cbb0137c59070c5e942830b
Nydun <juanandresferreira97@gmail.com>
2019-06-24 16:47:03 -0300

    arreglo conflitcto

--- java/BiosRent/src/java/MVC/modelo/persistencia/FabricaPersistencia.java
+++ java/BiosRent/src/java/MVC/modelo/persistencia/FabricaPersistencia.java
@@ -12,16 +12,15 @@ import MVC.modelo.persistencia.*;
  * @author Nicolas
  */
 public class FabricaPersistencia {
-<<<<<<< HEAD
+
     public static IPersistenciaCliente getPersistenciaCliente(){
         return PersistenciaCliente.getInstancia();
                 
     }
     public static IPersistenciaEmpleado getPersistenciaEmpleado(){
         return PersistenciaEmpleado.getInstancia();
-=======
+    }
     public static IPersistenciaAlquiler getPersistenciaAlquiler(){
-        return PersistenciaAlquiler.getInstancia();
->>>>>>> bcab0398e825bf7dd60379433b9948a286b2bca5
+        return PersistenciaAlquiler.getInstancia();   
     }
 }
--- java/BiosRent/src/java/MVC/presentacion/servlets/controladores/ControladorInicio.java
+++ java/BiosRent/src/java/MVC/presentacion/servlets/controladores/ControladorInicio.java
@@ -30,16 +30,16 @@ public class ControladorInicio extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-<<<<<<< HEAD
+
 
         
     
-=======
+
     private void index_get(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/vistas/inicio/index.jsp").forward(request, response);
     }
->>>>>>> bcab0398e825bf7dd60379433b9948a286b2bca5
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -53,78 +53,8 @@ public class ControladorInicio extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String accion = request.getParameter("accion");
-<<<<<<< HEAD
-                if(accion ==null) accion = "logIn";
-                switch(accion){
-                    case "logIn":
-                        logIn_get(request,response);
-                        break;
-                }
-        
-      
-    }
 
-     
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String accion = request.getParameter("accion");
-        switch(accion){
-            case "logIn":
-                logIn_post(request,response);
-                break;
-            case "logOut":
-                logOut_post(request,response);
-                break;
-        }
-        
-    }
-
-    private void logIn_get(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        if(request.getSession().getAttribute("empleadoLogueado")== null){
-            request.getRequestDispatcher("WEB-INF/vistas/empleado/login.jsp").forward(request, response);
-            
-        }else{
-            response.sendRedirect("vehiculo?accion=ver");
-        }
-    }
-
-    private void logIn_post(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        try {
-            String nomUser = request.getParameter("NombreUser");
-            String passUser = request.getParameter("Pass");
-            Empleado unEmp = FabricaLogica.getLogicaEmpleado().logueo(nomUser, passUser);
-            if(unEmp !=null){
-                request.getSession().setAttribute("empleadoLogueado",unEmp);
-                
-                Alquiler unAlquiler = new Alquiler();
-              //  unAlquiler.setEmpleado(unEmp);
-                request.getSession().setAttribute("alquiler", unAlquiler);
-                response.sendRedirect("vehiculo?accion=ver");
-                        
-            }else{
-                request.setAttribute("mensaje", "Las credenciales ingresadas no son correctas");
-                request.getRequestDispatcher("WEB-INF/vistas/empleado/login.jsp").forward(request, response);
-            }
-        }catch(ExcepcionPersonalizada ex){
-            request.setAttribute("mensaje", ex.getMessage());
-            request.getRequestDispatcher("WEB-INF/vistas/empleado/login.jsp").forward(request, response);
-        }catch (Exception e) {
-            request.setAttribute("mensaje", "No se pudo loguear el empleado");
-            request.getRequestDispatcher("WEB-INF/vistas/empleado/login.jsp").forward(request, response);
-        }
-    }
-
-    private void logOut_post(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getSession().removeAttribute("empleadoLogueado");
-        request.getSession().removeAttribute("alquiler");
-        response.sendRedirect("empleado?accion=logIn");
-        
-    }
-    
-=======
-
-        if (accion == null) {
+                  if (accion == null) {
             accion = "index";
         }
 
@@ -134,5 +64,13 @@ public class ControladorInicio extends HttpServlet {
                 break;
         }
     }
->>>>>>> bcab0398e825bf7dd60379433b9948a286b2bca5
+        
+      
+    
+
+     
+ 
+
+   
+
 }
